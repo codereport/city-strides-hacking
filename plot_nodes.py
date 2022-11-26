366b57268941bdfd683ca6206410adea787969fb
Conor Hoekstra <codereport@outlook.com>
2022-11-26 18:39:20 -0500

    Small updates

--- plot_nodes.py
+++ plot_nodes.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import plotly.express as px
 
+# TODO: https://plotly.com/python/lines-on-mapbox/
+#       Update to "go" so I can do traces & markers at same time
+
+# style = ("carto-darkmatter", "blue")
+style = ("stamen-terrain", "red")
+
 cities = pd.read_csv("nodes.csv")
 
 fig = px.scatter_mapbox(
@@ -10,12 +16,12 @@ fig = px.scatter_mapbox(
     size="sz",
     size_max=11,
     hover_name="names",
-    color_discrete_sequence=["blue"],
+    color_discrete_sequence=[style[1]],
     zoom=14,
     height=1300,
 )
 # 'open-street-map', 'white-bg', 'carto-positron',
 # 'carto-darkmatter', 'stamen-terrain', 'stamen-toner', 'stamen-watercolor'
-fig.update_layout(mapbox_style="carto-darkmatter")
+fig.update_layout(mapbox_style=style[0])
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 fig.show()

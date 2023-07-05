14f3690fc6ebd1a1958a05b64b4fe2289aad6f24
Conor Hoekstra <codereport@outlook.com>
2023-07-05 17:45:29 -0400

    REFACTOR: Shorten code

--- download_node_csv.py
+++ download_node_csv.py
@@ -45,15 +45,15 @@ except:
     print(f"Invalid input: Number must be <= {len(City)}")
     sys.exit()
 
-if   city == City.YORK:        lon_lats = [-79.3829, 43.7206, -79.5560, 43.6424]
-elif city == City.WROCLAW:     lon_lats = [17.078224311098552, 51.13988879756559,  17.00226573206399,  51.09263199227115 ]
-elif city == City.KRAKOW:      lon_lats = [19.979436306324942, 50.08859858611942,  19.90053987336441,  50.034804024531525]
-elif city == City.ROME:        lon_lats = [12.519622013860925, 41.91439535724936,  12.443743029831694, 41.85439499689079 ]
-elif city == City.VENICE:      lon_lats = [12.357666134722393, 45.451386491714715, 12.316599314442499, 45.42077976598716 ]
-elif city == City.FOLKESTONE:  lon_lats = [1.2028963028344322, 51.11176465501126,  1.1199095563368644, 51.05639602006997 ]
-else:                          lon_lats = [-79.27, 43.75, -79.49, 43.61]
+if   city == City.YORK:        coordinates = [-79.3829, 43.7206, -79.5560, 43.6424]
+elif city == City.WROCLAW:     coordinates = [17.078224311098552, 51.13988879756559,  17.00226573206399,  51.09263199227115 ]
+elif city == City.KRAKOW:      coordinates = [19.979436306324942, 50.08859858611942,  19.90053987336441,  50.034804024531525]
+elif city == City.ROME:        coordinates = [12.519622013860925, 41.91439535724936,  12.443743029831694, 41.85439499689079 ]
+elif city == City.VENICE:      coordinates = [12.357666134722393, 45.451386491714715, 12.316599314442499, 45.42077976598716 ]
+elif city == City.FOLKESTONE:  coordinates = [1.2028963028344322, 51.11176465501126,  1.1199095563368644, 51.05639602006997 ]
+else:                          coordinates = [-79.27, 43.75, -79.49, 43.61]
 
-[start_nelng, start_nelat, start_swlng, start_swlat] = lon_lats
+[start_nelng, start_nelat, start_swlng, start_swlat] = coordinates
 
 delta = 0.004 if city == City.VENICE else 0.012
 lng_tile = int(abs(start_nelng - start_swlng) // delta) + 1
@@ -79,17 +79,9 @@ for x in range(0, lat_tile):
         c = a - delta
         d = b - delta
 
-        params = {
-            "city": city.value,
-            "nelng": str(a),
-            "nelat": str(b),
-            "swlng": str(c),
-            "swlat": str(d),
-        }
-
-        grid = (str(a), str(b), str(c), str(d))
-        if grid not in cache:
-
+        coordinates = (str(a), str(b), str(c), str(d))
+        if coordinates not in cache:
+            params = { "city": city.value, "nelng": str(a), "nelat": str(b), "swlng": str(c), "swlat": str(d), }
             response = requests.get(
                 "https://citystrides.com/nodes.json",
                 params=params,
@@ -100,17 +92,15 @@ for x in range(0, lat_tile):
             temp = parse_nodes(response)
             print(len(temp))
             if len(temp) == 0:
-                cache.add(grid)
+                cache.add(coordinates)
 
             nodes = nodes + temp
-
-titles = [["lat", "lon", "sz", "names"]]
 
 import csv
 
 with open("nodes.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerows(titles + nodes)
+    writer.writerows([["lat", "lon", "sz", "names"]] + nodes)
 
 with open(cache_file, "w", newline="") as f:
     writer = csv.writer(f)

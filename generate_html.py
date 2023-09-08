a5b9dc4860ba52d744defd19386b239d5d858e97
Conor Hoekstra <codereport@outlook.com>
2023-09-08 19:40:04 -0400

    :sparkles: add generate_html.py

---
+++ generate_html.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+import plotly.express as px
+
+style  = "stamen-terrain"
+cities = pd.read_csv("nodes.csv")
+
+fig = px.scatter_mapbox(
+    cities,
+    lat="lat",
+    lon="lon",
+    size="sz",
+    size_max=10,
+    hover_name="names",
+    color="len_cat",
+    zoom=14,
+    height=1300,
+)
+
+fig.update_layout(mapbox_style=style)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+fig.write_html('heat_map.html')

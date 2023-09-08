7e99af673e5635a960825c3e1e2174957ebcb249
Conor Hoekstra <codereport@outlook.com>
2023-09-08 19:32:03 -0400

    :recycle: clean up

--- generate_heat_map.py
+++ generate_heat_map.py
@@ -1,19 +1,17 @@
 #! /usr/bin/env python3
 
 from itertools import chain
+import math
 import utils
+from tqdm import tqdm
 
-obj     = utils.load_json('north_york')
+obj     = utils.load_json('bangkok')
 nodes   = utils.node_dictionary(obj)
 streets = utils.street_dictionary(obj)
 
-a, b, c = set(), set(), set()
 lengths = {}
 for s, n in streets.items():
     tl = utils.total_distance_of_paths(n, nodes)
-    if   tl < 0.05: a |= set(chain(*n))
-    elif tl < 0.1:  b |= set(chain(*n))
-    elif tl < 0.2:  c |= set(chain(*n))
     print(f"{s}, Number of nodes: {len(set(chain(*n)))}, Length: {round(tl, 3)}")
     for node in set(chain(*n)):
         lengths[node] = tl
@@ -23,14 +21,22 @@ print(f"# of Nodes From Streets: {sum(len(s) for s in streets.values())}")
 print(f"# of Nodes From Query: {len([e for e in obj['elements'] if e['type'] == 'node'])}")
 print(len(nodes))
 
-nodes = []
-for e in obj['elements']:
+def is_close_to_node(node):
+    for lat, lon in nodes.values():
+        if math.isclose(node[0], lat, rel_tol=1e-6, abs_tol=1e-6) and \
+           math.isclose(node[1], lon, rel_tol=1e-6, abs_tol=1e-6):
+            return True
+    return False
+
+nodes_to_write = []
+points_d = utils.load_cache()
+print("Determining which nodes to show...")
+for e in tqdm(obj['elements']):
     if e['type'] == 'node':
         id = e['id']
-        len_cat = 'a' if id in a else 'b' if id in b else 'c' if id in c else 'd'
-        l = lengths[e['id']]
-        if (l < 1):
-            nodes.append([float(e['lat']), float(e['lon']), 2, f"\"Name: {e['id']}\"", l])
+        l = lengths[id]
+        point = (float(e['lat']), float(e['lon']))
+        if (l < 1) and utils.is_close(points_d, point, 0.01):
+            nodes_to_write.append([float(e['lat']), float(e['lon']), 2, f"\"Name: {id}\"", l])
 
-# copy and pasted from download_node_csv.py
-utils.write_nodes_csv(nodes)
+utils.write_nodes_csv(nodes_to_write)
